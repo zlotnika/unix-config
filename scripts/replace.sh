f58adf2c2c39a48901ed52ac7832adce287b7f5e
Alex Zlotnik <zlotnika@gmail.com>
2016-06-21 11:46:45 -0600

    Remove some weird stuff from replace

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -2,14 +2,14 @@
 # http://stackoverflow.com/questions/6758963/find-and-replace-with-sed-in-directory-and-sub-directories
 
 default_directory="./"
-default_restrict=""
+default_restrict="*"
 
 replace=$1
 with=$2
 directory_name=${3-$default_directory}
 restrict=${4-$default_restrict}
 
-find $directory_name -type f -exec sed -i "" "s#$replace#$with#g" $restrict {} \;
+find $directory_name -type f -exec sed -i "" "s#$replace#$with#g" {} \;
 
 
 ##### EXAMPLES #####
