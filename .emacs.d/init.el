c1c173506316ca0d7117d7d1d2193acf16094aba
Alex Zlotnik <zlotnika@gmail.com>
2016-10-17 12:57:10 -0700

    use column numbers in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,6 +79,7 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(setq column-number-mode t)
 
 ;; automatic pairs
 ;; (electric-pair-mode 1)
