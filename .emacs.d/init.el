5278df0172394a646b198f8f2d6e50fadcf6cd73
Alex Zlotnik <zlotnika@gmail.com>
2017-04-05 11:22:15 -0700

    Import js2 mode non-hackily

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -95,11 +95,11 @@
 ;; ruby ;;
 (setq ruby-deep-indent-paren nil)
 (setq ruby-deep-indent-paren-style nil)
+(setq compilation-scroll-output t)
 (add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Rakefile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Guardfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-(setq compilation-scroll-output t)
 
 ;; rspec ;;
 (setq rspec-use-vagrant-when-possible t)
@@ -114,6 +114,7 @@
 (add-to-list 'auto-mode-alist '("\\.yml.example\\'" . yaml-mode))
 
 ;; json ;;
+(add-hook 'json-mode-hook #'jscs-indent-apply)
 (add-to-list 'auto-mode-alist '("\\.jshintrc\\'" . json-mode))
 (add-to-list 'auto-mode-alist '("\\.jscsrc\\'" . json-mode))
 (add-to-list 'auto-mode-alist '("\\.ember-cli\\'" . json-mode))
@@ -132,66 +133,23 @@
 ;; handlebars ;;
 (require 'web-mode)
 (setq web-mode-markup-indent-offset 2)
-(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (setq web-mode-enable-auto-pairing t)
 (setq web-mode-enable-current-element-highlight t)
 (setq web-mode-enable-current-column-highlight t)
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
 ;; javascript ;;
 (setq javascript-indent-level 2)
 (setq js-indent-level 2)
 (setq js2-basic-offset 2)
-(add-to-list 'auto-mode-alist '("\\.js.erb\\'" . javascript-mode))
+(add-hook 'js-mode-hook #'jscs-indent-apply)
 (add-hook 'js2-mode-hook #'jscs-indent-apply)
+(add-hook 'js-mode-hook #'jscs-fix-run-before-save)
 (add-hook 'js2-mode-hook #'jscs-fix-run-before-save)
-
-;; js2 ;;
-;; https://yoo2080.wordpress.com/2012/03/15/js2-mode-setup-recommendation/
-(add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
-;; (add-hook 'js2-mode-hook 'my-disable-indent-tabs-mode)
-;; (defun my-disable-indent-tabs-mode ()
-;;     (set-variable 'indent-tabs-mode nil))
-;; (eval-after-load "js2-mode"
-;;   '(progn
-;;      (setq js2-missing-semi-one-line-override t)
-;;      (setq-default js2-basic-offset 2) ; 2 spaces for indentation (if you prefer 2 spaces instead of default 4 spaces for tab)
-
-;;      ;; add from jslint global variable declarations to js2-mode globals list
-;;      ;; modified from one in http://www.emacswiki.org/emacs/Js2Mode
-;;      (defun my-add-jslint-declarations ()
-;;        (when (> (buffer-size) 0)
-;;          (let ((btext (replace-regexp-in-string
-;;                        (rx ":" (* " ") "true") " "
-;;                        (replace-regexp-in-string
-;;                         (rx (+ (char "\n\t\r "))) " "
-;;                         ;; only scans first 1000 characters
-;;                         (save-restriction (widen) (buffer-substring-no-properties (point-min) (min (1+ 1000) (point-max)))) t t))))
-;;            (mapc (apply-partially 'add-to-list 'js2-additional-externs)
-;;                  (split-string
-;;                   (if (string-match (rx "/*" (* " ") "global" (* " ") (group (*? nonl)) (* " ") "*/") btext)
-;;                       (match-string-no-properties 1 btext) "")
-;;                   (rx (* " ") "," (* " ")) t))
-;;            )))
-;;           (add-hook 'js2-post-parse-callbacks 'my-add-jslint-declarations)))
-
-;; Flycheck JSCS
-;; (flycheck-def-config-file-var flycheck-jscs javascript-jscs ".jscsrc"
-;;   :safe #'stringp)
-;; (flycheck-define-checker javascript-jscs
-;;   "A JavaScript code style checker.
-;; See URL `https://github.com/mdevils/node-jscs'."
-;;   :command ("jscs" "--reporter" "checkstyle"
-;;             (config-file "--config" flycheck-jscs)
-;;             source)
-;;   :error-parser flycheck-parse-checkstyle
-;;   :modes (js-mode js2-mode js3-mode)
-;;   :next-checkers (javascript-jshint))
-;; (defun jscs-enable () (interactive)
-;;        (add-to-list 'flycheck-checkers 'javascript-jscs))
-;; (defun jscs-disable () (interactive)
-;;        (setq flycheck-checkers (remove 'javascript-jscs flycheck-checkers)))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js.erb\\'" . js2-mode))
 
 (provide 'init)
 ;;; init.el ends here
