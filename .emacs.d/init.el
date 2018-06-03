519683ddbdc67f5e96816b74763da42fab7bf8fc
Alex Zlotnik <zlotnika@gmail.com>
2018-06-02 20:11:02 -0700

    Switch to emacs gui

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -83,6 +83,8 @@
 ;; automatic pairs
 ;; (electric-pair-mode 1)
 
+(set-default-font "Andale Mono-18")
+
 ;;;;;;;;;;;
 ;; Modes ;;
 ;;;;;;;;;;;
@@ -161,6 +163,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark)))
  '(custom-safe-themes
    (quote
     ("19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" default)))
